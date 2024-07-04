1f46258949377336fedf7c5a841d957b8bef2c8f
Nicole Murch <nicolemurch@nicoles-mbp.lan>
2024-07-04 15:49:05 -0700

    Added Navbar and Navbar styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import Navbar from './components/TempNavbar';
+import Navbar from './components/Navbar';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 
 function App() {
