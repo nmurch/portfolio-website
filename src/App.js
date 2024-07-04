06ede6769a24a2837991965bb9305339e3e830f4
Nicole Murch <nicolemurch@nicoles-mbp.lan>
2024-07-04 14:53:37 -0700

    Rename TempNavbar.js to Navbar.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import Navbar from './components/Navbar';
+import Navbar from './components/TempNavbar';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 
 function App() {
