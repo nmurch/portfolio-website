b8012fcd92ad26e7cbf12c8e40b2f6f07a2e3457
Nicole Murch <nicolemurch@Nicoles-MacBook-Pro.local>
2024-07-06 19:51:27 -0700

    Fixed navbar display issues on mobile menu

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
 import { Button } from './Button';
@@ -19,18 +19,22 @@ function Navbar() {
     }
   };
 
+  useEffect(() => {
+    showButton();
+  }, []); 
+
   window.addEventListener('resize', showButton);
 
   return (
     <nav className='navbar'>
         <div className='navbar-container'>
-            <Link to='/' className='navbar-logo'>
+            <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
                 NMURCH <i className='fab fa-typo3' />
             </Link>
             <div className='menu-icon' onClick={handleClick}>
                 <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
             </div>
-            <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+            <ul className={click ? 'nav-menu active' : 'nav-menu inactive'}>
               <li className='nav-item'>
                   <Link to='/' className='nav-links' onClick={closeMobileMenu}>
                     Home
@@ -46,6 +50,15 @@ function Navbar() {
                     About
                   </Link>
               </li>
+              <li>
+              <Link
+                to='/contact'
+                className='nav-links-mobile'
+                onClick={closeMobileMenu}
+              >
+                CONTACT
+              </Link>
+            </li>
             </ul>
             {button && <Button buttonStyle='btn--outline'>CONTACT</Button>}
         </div>
